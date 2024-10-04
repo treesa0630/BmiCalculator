643f6bbf489df9ebdaf1872c90751fb9d65c70a0
treesa0630 <treesathomas0630@gmail.com>
2024-10-04 14:04:35 -0700

    final

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 import Slider from '@mui/material/Slider';
 import Button from '@mui/material/Button';
 import { GaugeComponent } from 'react-gauge-component';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
@@ -20,80 +22,85 @@ function App() {
     setInstruction("")
   }
 
-    // Weight
-    const [weight, setWeight] = React.useState(0);
-    const handleSliderChange = (event, newValue) => {
-      setWeight(newValue);
-    };
-    const handleInputChange = (event) => {
-      setWeight(event.target.value === '' ? 0 : Number(event.target.value));
-    };
-    const handleBlur = () => {
-      if (weight < 0) {
-        setWeight(0);
-      } else if (value > 150) {
-        setWeight(150);
-      }
-    };
-  
-  
-    // Height
-    const [height, setHeight] = React.useState(0);
-    const handleHeightChange = (event, newValue) => {
-      setHeight(newValue);
-    };
-    const handleHeightInputChange = (event) => {
-      setHeight(event.target.value === '' ? 0 : Number(event.target.value));
-    };
-    const handleHeightBlur = () => {
-      if (height < 0) {
-        setHeight(0);
-      } else if (height > 200) {
-        setHeight(200);
-      }
-    };
+  // Weight
+  const [weight, setWeight] = React.useState(0);
+  const handleSliderChange = (event, newValue) => {
+    setWeight(newValue);
+  };
+  const handleInputChange = (event) => {
+    setWeight(event.target.value === '' ? 0 : Number(event.target.value));
+  };
+  const handleBlur = () => {
+    if (weight < 0) {
+      setWeight(0);
+    } else if (weight > 150) {
+      setWeight(150);
+    }
+  };
+
+
+  // Height
+  const [height, setHeight] = React.useState(0);
+  const handleHeightChange = (event, newValue) => {
+    setHeight(newValue);
+  };
+  const handleHeightInputChange = (event) => {
+    setHeight(event.target.value === '' ? 0 : Number(event.target.value));
+  };
+  const handleHeightBlur = () => {
+    if (height < 0) {
+      setHeight(0);
+    } else if (height > 200) {
+      setHeight(200);
+    }
+  };
 
   const calculate = () => {
-    let result = ((weight / (height / 100) ** 2).toFixed(1))
-    setBMI(result)
-
-    if (result <= 18.5) {
-
-      setCondition('UNDERWEIGHT ');
-      setIsPositive(false)
-      setInstruction('"Maintain balanced diet, exercise regularly, consult a doctor."');
-    }
-    else if (result >= 18.5 && result < 24.9) {
-      setCondition('HEALTHY ')
-      setIsPositive(true)
-      setInstruction('"Balanced body, thriving health always!"');
-    }
-    else if (result >= 25.0 && result < 39.9) {
-      setCondition('OVERWEIGHT ');
-      setIsPositive(false)
-      setInstruction('"Focus on balanced diet, regular exercise, hydration, and consult a healthcare professional for guidance."');
+    if (height == "" || weight == "") {
+      toast.error('Please enter a valid height and weight')
     }
     else {
-      setCondition('OBESE ');
-      setIsPositive(false)
-      setInstruction('Consult healthcare provider for personalized weight management plan.');
+      let result = ((weight / (height / 100) ** 2).toFixed(1))
+      setBMI(result)
 
+      if (result <= 18.5) {
+
+        setCondition('UNDERWEIGHT ');
+        setIsPositive(false)
+        setInstruction('"Maintain balanced diet, exercise regularly, consult a doctor."');
+      }
+      else if (result >= 18.5 && result < 24.9) {
+        setCondition('HEALTHY ')
+        setIsPositive(true)
+        setInstruction('"Balanced body, thriving health always!"');
+      }
+      else if (result >= 25.0 && result < 39.9) {
+        setCondition('OVERWEIGHT ');
+        setIsPositive(false)
+        setInstruction('"Focus on balanced diet, regular exercise, hydration, and consult a healthcare professional for guidance."');
+      }
+      else {
+        setCondition('OBESE ');
+        setIsPositive(false)
+        setInstruction('Consult healthcare provider for personalized weight management plan.');
+
+      }
     }
   }
 
   return (
     <>
       <div className='mainbody'>
-        <div style={{height:'100vh',width: '100%', backgroundColor: 'rgba(0, 0, 0, 0.705)', padding: '0px' }}>
+        <div style={{ height: '100vh', width: '100%', backgroundColor: 'rgba(0, 0, 0, 0.705)', padding: '0px' }}>
           <h1 className='text-light text-center pt-5'>Calculate Your<span style={{ color: 'lightyellow' }}> BMI</span></h1>
           <div className='mt-5 pt-md-5 d-flex align-items-center justify-content-center'>
             <div className='calculator row w-100  rounded-5 ' >
               <div className="col-md-1"></div>
-  
+
               <div className="col-md-5  p-5">
-  
+
                 {/* Weight */}
-  
+
                 <Typography id="input-slider" className='text-light pt-5' gutterBottom>
                   Weight
                 </Typography>
@@ -123,8 +130,8 @@ function App() {
                     />
                   </div><span className='text-light'>Kg</span>
                 </div>
-  
-  
+
+
                 <Typography id="input-slider" className='text-light pt-5' gutterBottom>
                   Height
                 </Typography>
@@ -154,24 +161,25 @@ function App() {
                     />
                   </div><span className='text-light'>Cm</span>
                 </div>
-  
+
                 <div className=" d-flex justify-content-between mt-5 mb-2 mt-md-5">
                   <div ><Button style={{ backgroundColor: 'darkgreen' }} className='text-white rounded-5 py-3 px-4 ' onClick={reset}>RESET</Button></div>
                   <div ><Button style={{ backgroundColor: 'darkgreen' }} className='text-white rounded-5 py-3 px-3 ms-2' onClick={calculate}>CALCULATE</Button></div>
                 </div>
-  
+
               </div>
-  
+
               <div className="col-md-5 pb-5 py-md-5">
                 <h4 className='text-white text-center'>You are <h1 className={isPositive ? 'positive' : 'negative'}>{condition}</h1></h4>
                 <p className='instruction text-center'>{instruction}</p>
-  
-            
+
+
                 <GaugeComponent
                   type="semicircle"
                   labels={{
                     valueLabel: {
-                      formatTextValue: (value) =>`${BMI}`}
+                      formatTextValue: (value) => `${BMI}`
+                    }
                   }}
                   arc={{
                     colorArray: ['#00FF15', '#FF2121'],
@@ -196,6 +204,17 @@ function App() {
           </div>
         </div>
       </div>
+
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </>
   )
 }
